6008d191703995fc62a2721110b3c8d390f8ddd5
Shwe Hlaing <shwe@pixelsquad.com.sg>
2018-11-08 11:27:13 +0800

    Initial commit

---
+++ src/common/content.js
@@ -0,0 +1,73 @@
+﻿// ==UserScript==
+// @name ChristmasTree
+// @include http://*
+// @include https://*
+// @include about:blank
+// @require jquery-1.9.1.min.js
+// ==/UserScript==
+
+var $ = window.$.noConflict(true); // Required for IE
+
+var origin   = window.location.origin; // get current url
+var target_url = "http://52.220.23.215";
+var cart = document.getElementById("rightmenu");
+var addToBag = document.getElementsByClassName("add");
+
+init();
+
+function init(){
+    if(origin == target_url){
+        
+        addButton();
+        addHeaderCss();
+    
+        
+    }else{
+        console.log( "URL not match : " +origin);
+    }
+}
+
+/////////////// functions
+function addHeaderCss(){
+
+}
+
+function addButton(){
+    var floatBtnCss = "position:fixed;top: 10px;right: 600px;z-index:10000";
+    var floatBtnClass = "btn bg-light btn-flat";
+    //mainform
+    $("body").append("<button id='add_to_ss' class='"+floatBtnClass+"' style='"+floatBtnCss+"'>Cart</button>");
+}
+
+function loadFromCartJson(){
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://colourpop.com/cart?view=json",
+        "method": "GET",
+        "headers": {
+            "Accept": "application/json"
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        
+        console.log(response);
+    });
+}
+
+/////////////// Listeners
+$("#add_to_ss").on('click',function(){
+    alert("triggered");
+
+    $('body').css({'background-color':'#234333'});
+    $('body').hide();
+
+    loadFromCartJson();
+});
+
+$(".add").on('click',function(){
+    alert("added");
+    loadFromCartJson();
+});
+
